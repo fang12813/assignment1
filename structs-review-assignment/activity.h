416e9decfe76e1f1c070a22919ac12e54a0a90ca
fang12813 <fang.huang1@pcc.edu>
2025-04-06 12:28:20 -0700

    Update activity.h

--- structs-review-assignment/activity.h
+++ structs-review-assignment/activity.h
@@ -6,10 +6,27 @@
 // v.type: athletic, food, arts, games, or others - have about 5 different types of
 // activities for the user to choose from
 
+#ifndef ACTIVITY_H
+#define ACTIVITY_H
+
+#include <iostream>
+#include <cstddef>  // for size_t(ype)
+using namespace std;
+
 class Activity {  // can also use stuct activity
      public:      // only if using class keyword
      static const size_t MAX_CHARS = 50;
-     char* name;
+
+     char* name[MAX_CHARS + 1] = {0};
+     char location[MAX_CHARS + 1] = {0};
+     char level[MAX_CHARS + 1] = {0};
+     int rating;   // 1-10
+     int type;     // 0-4
      
 };
+
+void print(ostream &os, Activity &it);
+void printAll(ostream &os, Activity arr[], size_t size);
+
+#endif
 
