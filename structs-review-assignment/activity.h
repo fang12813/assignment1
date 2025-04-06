d7dc6df4a496c076f9286cd70cc794fec75597ba
fang12813 <fang.huang1@pcc.edu>
2025-04-06 11:56:42 -0700

    Update activity.h

---
+++ structs-review-assignment/activity.h
@@ -0,0 +1,15 @@
+// for each activity, you need to keep track of
+// activity name(e.g., skiing)
+//ii. activity location (e.g., mt hood meadows)
+// iii.activity level (e.g.l, easy, difficult, not for the faint of heart etc)
+// iv.rating(e.g., 1-10 about how fun the experience is)
+// v.type: athletic, food, arts, games, or others - have about 5 different types of
+// activities for the user to choose from
+
+class Activity {  // can also use stuct activity
+     public:      // only if using class keyword
+     static const size_t MAX_CHARS = 50;
+     char* name;
+     
+};
+
